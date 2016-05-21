07921fe624c7e967679be5543fd71eadaca76f7f
James Reilly <reillyj33@gmail.com>
2016-05-21 15:19:35 +0100

    Seperate ScoreUtils logic into seperate spec
    The spec was getting a little large and hard to read

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -71,49 +71,10 @@ RSpec.describe Board do
     end
   end
 
-  # Get a report of potential winning lines ordered by moves and grouped by marker
-  describe '#winning_lines' do
-    let(:expected_o_output) do
-      [
-        %w(2 b ), %w(3 a ), %w(1 b ), %w(2 c ), %w(1 a ), %w(1 a ), %w(3 b ), %w(3 c ),
-        %w(3 c ), %w(1 b ), %w(2 b ), %w(2 c ), %w(3 a ), %w(3 b )
-      ]
-    end
-    let(:expected_x_output) do
-      [
-        %w(3 a ), %w(2 b ), %w(1 b ), %w(1 c ), %w(2 c ), %w(2 a ), %w(2 b ), %w(2 c ),
-        %w(1 a ), %w(3 a ), %w(1 a ), %w(1 b ), %w(1 c ), %w(2 a )
-      ]
-    end
-    it 'reports the winning lines grouped by marker and ordered by moves' do
-      board.set_position('o', '1', 'c')
-      board.set_position('o', '2', 'a')
-      board.set_position('x', '3', 'b')
-      board.set_position('x', '3', 'c')
-      expect(board.winning_lines('o')).to eq expected_o_output
-      expect(board.winning_lines('x')).to eq expected_x_output
-    end
-  end
-
   describe '#oppponent_marker' do
     it 'retirves the opponent marker for a given marker' do
       expect(board.opponent_marker('x')).to eq 'o'
       expect(board.opponent_marker('o')).to eq 'x'
-    end
-  end
-
-  describe '#winning_line?' do
-    it 'return true for a winning line' do
-      board.set_position('o', '1', 'a')
-      board.set_position('o', '1', 'b')
-      board.set_position('o', '1', 'c')
-      expect(board.winning_line?('o')).to eq true
-    end
-
-    it 'return false for a winning line' do
-      board.set_position('o', '1', 'a')
-      board.set_position('o', '1', 'b')
-      expect(board.winning_line?('o')).to eq false
     end
   end
 end
---
+++ spec/score_utils_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+# Test module ScoreUtils
+RSpec.describe ScoreUtils do
+  let(:board) { Board.new }
+
+  # Get a report of potential winning lines ordered by moves and grouped by marker
+  describe '#winning_lines' do
+    let(:expected_o_output) do
+      [
+        %w(2 b ), %w(3 a ), %w(1 b ), %w(2 c ), %w(1 a ), %w(1 a ), %w(3 b ), %w(3 c ),
+        %w(3 c ), %w(1 b ), %w(2 b ), %w(2 c ), %w(3 a ), %w(3 b )
+      ]
+    end
+    let(:expected_x_output) do
+      [
+        %w(3 a ), %w(2 b ), %w(1 b ), %w(1 c ), %w(2 c ), %w(2 a ), %w(2 b ), %w(2 c ),
+        %w(1 a ), %w(3 a ), %w(1 a ), %w(1 b ), %w(1 c ), %w(2 a )
+      ]
+    end
+    it 'reports the winning lines grouped by marker and ordered by moves' do
+      board.set_position('o', '1', 'c')
+      board.set_position('o', '2', 'a')
+      board.set_position('x', '3', 'b')
+      board.set_position('x', '3', 'c')
+      expect(board.winning_lines('o')).to eq expected_o_output
+      expect(board.winning_lines('x')).to eq expected_x_output
+    end
+  end
+
+  describe '#winning_line?' do
+    it 'return true for a winning line' do
+      board.set_position('o', '1', 'a')
+      board.set_position('o', '1', 'b')
+      board.set_position('o', '1', 'c')
+      expect(board.winning_line?('o')).to eq true
+    end
+
+    it 'return false for a winning line' do
+      board.set_position('o', '1', 'a')
+      board.set_position('o', '1', 'b')
+      expect(board.winning_line?('o')).to eq false
+    end
+  end
+end
